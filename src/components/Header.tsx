5716729967b519d300b1f5c8ddcec9d0f1b5069a
Hugo Caillard <hg.caillard@gmail.com>
2022-03-29 16:32:24 +0200

    step-1 simplify localstorage handling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,16 @@ import { Container } from './UI/Container'
 import { H1 } from './UI/Typography'
 
 export const Header = () => {
-  const { connect, disconnect, session, isLoading } = useAuth()
+  const { connect, disconnect, session } = useAuth()
 
   return (
     <header>
       <Container className="h-16 flex justify-between items-center">
         <H1>Color App</H1>
-        {!isLoading && (
-          <>
-            {session ? (
-              <Button onClick={disconnect}>Disconnect</Button>
-            ) : (
-              <Button onClick={connect}>Connect Wallet</Button>
-            )}
-          </>
+        {session ? (
+          <Button onClick={disconnect}>Disconnect</Button>
+        ) : (
+          <Button onClick={connect}>Connect Wallet</Button>
         )}
       </Container>
     </header>
