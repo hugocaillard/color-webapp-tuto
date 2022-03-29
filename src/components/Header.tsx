40f6bb44210e1df3f885bfc68bf462ee6cf91562
Hugo Caillard <hg.caillard@gmail.com>
2022-03-29 16:39:27 +0200

    improve ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { H1 } from './UI/Typography'
 export const Header = () => {
   return (
     <header>
-      <Container className="flex justify-between items-center">
+      <Container className="h-16 flex justify-between items-center">
         <H1>Color App</H1>
         <Button>Connect Wallet</Button>
       </Container>
--- src/components/UI/Typography.tsx
+++ src/components/UI/Typography.tsx
@@ -3,3 +3,11 @@ import { FunctionalComponent } from 'preact'
 export const H1: FunctionalComponent = ({ children }) => {
   return <h1 className="text-slate-900 text-3xl font-semibold">{children}</h1>
 }
+
+export const H2: FunctionalComponent = ({ children }) => {
+  return <h2 className="text-slate-900 text-2xl font-medium">{children}</h2>
+}
+
+export const H3: FunctionalComponent = ({ children }) => {
+  return <h3 className="text-slate-800 text-xl font-medium">{children}</h3>
+}
