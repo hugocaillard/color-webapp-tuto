f20299d1570b0ff082031daf1201934d13eb2c81
Hugo Caillard <hg.caillard@gmail.com>
2022-03-29 16:32:24 +0200

    step-1 simplify localstorage handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'preact/hooks'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Container } from './components/UI/Container'
@@ -6,10 +5,7 @@ import { useAuth } from './hooks/useAuth'
 import { Vote } from './pages/Vote'
 
 export function App() {
-  const { connectFromStorage, session, isLoading } = useAuth()
-  useEffect(() => {
-    connectFromStorage()
-  }, [])
+  const { session } = useAuth()
 
   return (
     <div className="h-screen flex flex-col text-slate-800">
@@ -17,11 +13,9 @@ export function App() {
         <Header />
 
         <main>
-          {!isLoading && (
-            <Container>
-              {session ? <Vote /> : <p>Connect your wallet</p>}
-            </Container>
-          )}
+          <Container>
+            {session ? <Vote /> : <p>Connect your wallet</p>}
+          </Container>
         </main>
       </div>
 
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,42 +1,35 @@
 import { StacksSessionState, authenticate } from 'micro-stacks/connect'
 import create from 'zustand'
 
+interface AuthStore {
+  session: StacksSessionState | null
+  connect: () => Promise<void>
+  disconnect: () => void
+}
+
 export const appDetails = {
   name: 'Colors Vote Tuto',
   icon: `localhost:3003/src/favicon.svg`,
 }
 
 const SESSION_STORAGE_KEY = 'stacks-session'
-const rawSession = localStorage.getItem(SESSION_STORAGE_KEY)
-
-interface AuthStore {
-  session: StacksSessionState | null
-  isLoading: boolean
-  connectFromStorage: () => void
-  connect: () => Promise<void>
-  disconnect: () => void
+let rawSession = localStorage.getItem(SESSION_STORAGE_KEY)
+let session: AuthStore['session'] = null
+if (rawSession) {
+  try {
+    session = JSON.parse(rawSession)
+  } catch (err) {
+    rawSession = null
+    localStorage.removeItem(SESSION_STORAGE_KEY)
+  }
 }
 
 export const useAuth = create<AuthStore>((set) => ({
-  session: null,
-  isLoading: true,
+  session,
 
   disconnect: () => {
     set({ session: null })
     localStorage.clear()
-  },
-
-  connectFromStorage: () => {
-    if (rawSession) {
-      try {
-        const session = JSON.parse(rawSession)
-        set({ session, isLoading: false })
-      } catch (err) {
-        console.warn(err)
-      }
-    } else {
-      set({ isLoading: false })
-    }
   },
 
   connect: async () => {
