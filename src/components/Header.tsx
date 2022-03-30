f96644be7b85c1efc8e6dc5f62e9fd212aa326e0
Hugo Caillard <hg.caillard@gmail.com>
2022-03-30 17:32:45 +0200

    step-2: simplify vote page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,6 @@ import { H1 } from './UI/Typography'
 export const Header = () => {
   const { connect, disconnect, session } = useAuth()
 
-  console.log('header rendering')
-
   return (
     <header>
       <Container className="h-16 flex justify-between items-center">
